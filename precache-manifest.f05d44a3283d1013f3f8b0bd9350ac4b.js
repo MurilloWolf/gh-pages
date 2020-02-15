a8ef0384521a270f897e88095fbe7abe0f7b592a
MurilloWolf <mwolfcavalheiro@gmail.com>
2020-02-14 23:28:33 -0300

    Updates

---
+++ precache-manifest.f05d44a3283d1013f3f8b0bd9350ac4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d5408270d38f7056b98073522bbc62",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "2306d7b4153e682920a7",
+    "url": "/gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a5259c6b01c921bbb9d",
+    "url": "/gh-pages/static/js/2.dcfc4e55.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gh-pages/static/js/2.dcfc4e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2306d7b4153e682920a7",
+    "url": "/gh-pages/static/js/main.b126bb11.chunk.js"
+  },
+  {
+    "revision": "8593d1deecd3b47dc1dd",
+    "url": "/gh-pages/static/js/runtime-main.096ccc68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
